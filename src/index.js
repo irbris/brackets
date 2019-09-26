5a499b9af99e5d04d5a86ec405d3ea54016cede6
irbris <55032489+irbris@users.noreply.github.com>
2019-09-26 12:08:34 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const arr = str.split("");
+  const num = 0;
+  if(str.length % 2 !== 0) return false;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (arr[i] === bracketsConfig[j][1] &&
+          res[res.length - 1] === bracketsConfig[j][0]){
+        res.pop(arr[i]);
+      } else if (arr[i] === bracketsConfig[j][0]) {
+        res.push(arr[i]);
+      } 
+    }    
+  }
+  return res.length === 0;
 }
